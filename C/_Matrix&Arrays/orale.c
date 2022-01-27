439cd30b27fe28f60f4dde0d6c6c94bd60764bbe
SamueleStabile <samuelestabilebusiness@gmail.com>
2022-01-27 11:37:55 +0100

    gl for exam goSamu

--- C/_Matrix&Arrays/orale.c
+++ C/_Matrix&Arrays/orale.c
@@ -12,25 +12,24 @@ int main() {
  leggi(fp, &dimrig, &dimcol, matrix);
 
     printf("RIGA: %d \n", dimrig);
-    printf("COLONNA: %d \n", dimcol);
+    printf("COLONNA: %d ", dimcol);
 
-    printf("TEST SE VEDE LA MATRICE NEL MAIN -->[%d]", matrix[0][0]);
+    printf("\nTEST SE VEDE LA MATRICE NEL MAIN -->[%d]", matrix[0][0]);
 
 }
 
 void leggi(FILE* fpTEMP, int *rig, int *col, int mat[][10]) {
 
-    fscanf(fpTEMP, "%d\n", rig);
+    fscanf(fpTEMP, "%d ", rig);
     fscanf(fpTEMP, "%d ", col);
 
 
     for(int i = 0; i < *rig; i++) {
         for(int j = 0; j < *col; j++){
-            fscanf(fpTEMP, "%d ", &mat[i][j]);
+            fscanf(fpTEMP, "%d", &mat[i][j]);
             printf("[%d]", mat[i][j]);
         }
         printf("\n");
     }
-
 }
 
