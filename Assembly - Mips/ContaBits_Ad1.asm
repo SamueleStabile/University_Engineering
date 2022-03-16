07564eafcf01d9c6be57a42c4778e62b1f2ae97e
SamueleStabile <samuelestabilebusiness@gmail.com>
2022-03-16 18:25:23 +0100

    Conta bit ad 1
    Funziona lez gooo

--- Assembly - Mips/ContaBits_Ad1.asm
+++ Assembly - Mips/ContaBits_Ad1.asm
@@ -6,7 +6,7 @@
 
 		.text
 	#inizializzo registri per il programma
-	li $t0, 0x1A75B380
+	li $t0, 0x1A75B380					
 	li $t1, 0
 
 	#altri registri utilizzati per "print"
@@ -14,14 +14,14 @@
 	la $a0, str
 	syscall
 	
-loop: 	beqz $t0 next
-      	andi $t2, $t0, 1
-      	beqz $t2, next2
-      	addi $t1, $t1, 1
-next2: 	srl $t0, $t0, 1
-	j loop
+loop: 	beqz $t0, next		#controllo se il registro $t0 é tutto 0
+      	andi $t2, $t0, 1	#faccio un and per confrontare LSB di t0 ed 1, assorbendo in t2 il risultato (che sará 1 o 0, in base all LSB di t0)
+      	beqz $t2, ifzero	#Se t2 é zero vuol dire che l'LSB corrente di t0 é 0, e quindi non devo contarlo e salto al label ifzero
+      	addi $t1, $t1, 1 	#incremento il registro contatore t1 
+ifzero: srl $t0, $t0, 1		#Shift logico a destra di $t0 cosí facendo scorro il registro per trovarmi un nuovo valore al LSB e scartare quello vecchio
+	j loop			#salto incodizionato al label loop
 	
-next: 	li $v0, 1
-	move $a0, $t0
+next: 	li $v0, 1		#stampo
+	move $a0, $t1		
 	syscall 
       
