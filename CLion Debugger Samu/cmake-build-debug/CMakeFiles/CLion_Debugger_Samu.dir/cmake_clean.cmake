2a006b9e4df003b633011ed032ffa37889e7746b
SamueleStabile <samuelestabilebusiness@gmail.com>
2022-09-28 17:11:28 +0200

    c

---
+++ CLion Debugger Samu/cmake-build-debug/CMakeFiles/CLion_Debugger_Samu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CLion_Debugger_Samu.exe"
+  "CLion_Debugger_Samu.exe.manifest"
+  "CLion_Debugger_Samu.pdb"
+  "CMakeFiles/CLion_Debugger_Samu.dir/main.c.obj"
+  "CMakeFiles/CLion_Debugger_Samu.dir/main.c.obj.d"
+  "libCLion_Debugger_Samu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLion_Debugger_Samu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
