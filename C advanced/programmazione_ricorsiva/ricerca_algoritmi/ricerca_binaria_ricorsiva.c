dcba10f266e545bcc4c02483c7f70a2ba5af0298
SamueleStabile <samuelestabilebusiness@gmail.com>
2022-10-05 12:14:41 +0200

    f

---
+++ C advanced/programmazione_ricorsiva/ricerca_algoritmi/ricerca_binaria_ricorsiva.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 300
+
+int binary_search(int *a[], int n, int x){
+int first = ;
+int chosen = (first + last)/2;
+
+}
+
+
+int main(int argc, char** argv){
+int a = {1,2,3,4,5,6,7,8,9,10};
+int x = 5;
+int n = 10;
+};
