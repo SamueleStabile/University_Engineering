99106fc7cb2ba177af54354ba189502194cbf8fe
Samuele Stabile <77798622+SamueleStabile@users.noreply.github.com>
2023-04-15 19:54:27 +0200

    Fork by Ubuntu (exec on ubuntu)

---
+++ Operating Systems/fork.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+
+	pid_t pid;
+
+	//create the child process
+	pid = fork();
+
+	if(pid < 0){
+		//error occurred
+		fprintf(stderr, "Fork Failed\n");
+	}
+
+	else if(pid==0){
+		//child process
+		fprintf(stdout, "I am the child process: [%d]\n", pid);
+		execlp( "bin/ls", "ls", NULL);
+	}
+	
+	else{
+		//parent process
+		fprintf(stdout, "I am the parent process: [%d]\n",pid);
+		//parent process wait for the child to complete
+		wait(NULL);
+		fprintf(stdout, "Child completed\n");
+	}
+	printf("Program Completed\n");
+	return EXIT_SUCCESS;
+	}
+
