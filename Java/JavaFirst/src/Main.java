913ca61e865434c1bb72d2890cda9cf8f59cb5c9
SamueleStabile <samuelestabilebusiness@gmail.com>
2023-09-29 11:08:47 +0200

    Java

--- Java/JavaFirst/src/Main.java
+++ Java/JavaFirst/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Prodotto p1 = new Prodotto("001" , "Nero", 9);
+        Prodotto p2 = new Prodotto("002" , "Bianco", 9);
+        Prodotto p3 = new Prodotto("003" , "Gialle", 90);
+
+        Prodotto prodotti[] = new Prodotto[3];
+
+        prodotti[0] = p1;
+        prodotti[1] = p2;
+        prodotti[2] = p3;
+
+        for( int i = 0; i < prodotti.length; i++){
+            prodotti[i].printProdotto();
+        }
     }
 }
