14114811aa085669168bab7d5a5e13c39b211fda
SamueleStabile <samuelestabilebusiness@gmail.com>
2023-09-29 12:23:50 +0200

    Java

--- Java/Studenti/src/App.java
+++ Java/Studenti/src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Studente s = new Studente("Francesco" , "Spinelli" , "6931");
+        Studente s1 = new Studente("Samuele" , "Stabile" , "6931");
+        Studente s2 = new Studente("Marco" , "Novissimo" , "6931");
+
+        System.out.println(s.getNome() + s.getCognome() + s.getMatricola());
+        System.out.println("Info\n");
+
+        System.out.println(s.getInfoStudente());
+        System.out.println(s1.getInfoStudente());
+        System.out.println(s2.getInfoStudente());
+
+        
+        Aula M = new Aula(150);
+
+        System.out.println( M.getMaxPosti());
+
+        M.entrataStudente(s);
+        M.entrataStudente(s1);
+        M.entrataStudente(s2);
+        M.elencoStudenti();
+
     }
 }
