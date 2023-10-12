56d7d51b4222a51132ad121c75cd6869cfb83dd9
SamueleStabile <samuelestabilebusiness@gmail.com>
2023-10-12 16:18:32 +0200

    Veicoli

---
+++ Java/Auto/src/TestVeicoli.java
@@ -0,0 +1,47 @@
+/*
+specificare direttive package e import
+ */
+
+
+/**
+ *
+ * @author luca
+ */
+public class TestVeicoli {
+
+    public static void main(String args[]){
+
+        Veicolo v;
+        System.out.println("Test 1: camion con targa valida");
+        v = new Camion("sdf244", "Fiat CX45", "Gasolio", "TT656671", 4);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+        System.out.println("Test 2: camion con targa non valida");
+        v = new Camion("spl265", "Volvo PTG", "Gasolio", "YH96671", 6);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+        System.out.println("Test 3: autovettura con targa  valida");
+        v = new Autovettura("mk23t", "Fiat Punto", "Metano", "EA566FM", 5);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+        System.out.println("Test 4: autovettura con targa non valida");
+        v = new Autovettura("cgt612", "Fiat Idea", "Gasolio", "AQ9Y7UUU", 5);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+        System.out.println("Test 5: moto con targa  valida");
+        v = new Moto("das7896", "Honda Hornet", "Benzina", "AT51233", false);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+        System.out.println("Test 6: moto con targa non valida");
+        v = new Moto("gdt7896", "Suzuki Bandit", "Benzina", "AT5123N", false);
+        System.out.println(v);
+        System.out.println("Targa valida? " + v.controllaTarga() + "\n\n");
+
+    }
+
+}
---
+++ Java/Auto/src/Veicolo.java
@@ -0,0 +1,15 @@
+public abstract class Veicolo {
+    private String numTelaio;
+    private String modello;
+    private String alimentazione;
+    private String targa;
+
+    public Veicolo (String numTelaio, String modello, String alimentazione, String targa){
+        this.numTelaio = numTelaio;
+        this.modello = modello;
+        this.alimentazione = alimentazione;
+        this.targa = targa;
+    }
+}
+
+
